162a4eda7dd2fb8e86bae8ecce34026da91a07ef
zatorriusf <zatorrius.furniss@outlook.com>
2020-07-27 19:07:45 -0400

    Removed test case

--- quick-sort.js
+++ quick-sort.js
@@ -15,7 +15,3 @@ function quickSort(array) {
     return [...quickSort(lowerBound) ,pivotVal ,...quickSort(upperBound)];
   }
   
-  const tst = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]
-  
-  console.log(quickSort(tst));
-  console.log(tst)
